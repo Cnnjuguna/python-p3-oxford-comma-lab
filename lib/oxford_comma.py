f7b87b748c5f5456eb642b9642f31c07da3fee6c
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-21 16:15:30 +0300

    Completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,40 @@
+""" If the list has only 1 element, return that element as a string.
+    If the list has 2 elements, join them with "and" and return the result.
+    If the list has 3 or more elements:
+    a. Join all elements except the last one with commas.
+    b. Add "and" before the last element.
+    c. Return the formatted string.
+"""
+
+
+# def oxford_comma(items):
+#     # only 1 element
+#     if len(items) == 1:
+#         return items[0]
+#     elif len(items) == 2:
+#         return f"{items[0]} and {items[1]}"
+#     elif len(items) == 3:
+#         formatted_item = ", ".join(items[:-1])  # joining the elements except the last one with a comma (,)
+#         # formatted_item += f", and {items[-1]}"  # add ("and") before the last item in the list
+#         formatted_item += f", and {items[-1]}"
+
+#         return formatted_item
+
+
+# formatted_list = oxford_comma(["apple", "banana", "cherry", "date"])
+# print(formatted_list)
+
+
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    elif len(items) >= 3:
+        formatted_items = ", ".join(items[:-1])  # joining the elements except the last one with a comma (,)
+        formatted_items += f", and {items[-1]}"  # add ("and") before the last item in the list
+        return formatted_items
+
+
+# Test cases
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))  # Output: "fiddleheads, okra, and kohlrabi"
